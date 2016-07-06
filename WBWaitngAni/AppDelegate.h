93fd5df6548795079a687a6ff4384eaa8e26819d
ulovebin <ulovewb@163.com>
2016-07-06 14:23:21 +0800

    Initial Commit

---
+++ WBWaitngAni/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBWaitngAni
+//
+//  Created by tuhui－03 on 16/7/6.
+//  Copyright © 2016年 wb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WBWaitngAni/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBWaitngAni
+//
+//  Created by tuhui－03 on 16/7/6.
+//  Copyright © 2016年 wb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
